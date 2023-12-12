bd65926da60da5c47d08c1f62e305984a6c243da
Aleksey Mukhanov <alekseythe1st@yandex.ru>
2023-12-12 14:33:43 +0600

    fix

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['84.252.136.51', '127.0.0.1', 'localhost', 'tskitskprctcm.ddns.net']
+ALLOWED_HOSTS = ['84.252.136.51', '127.0.0.1', 'tskitskprctcm.ddns.net']
 
 
 # Application definition
